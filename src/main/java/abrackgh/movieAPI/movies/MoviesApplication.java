ce8efa93a45c748a4439832c00b451aada572b34
Alex Brackenridge <a.brack@live.co.uk>
2023-01-25 20:48:50 +0000

    Initial commit

---
+++ src/main/java/abrackgh/movieAPI/movies/MoviesApplication.java
@@ -0,0 +1,13 @@
+package abrackgh.movieAPI.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
